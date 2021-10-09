7d8eeb31a72064cdba655369c8887ff8d8976e60
Tosuj <sanches00799@gmila.com>
2021-10-09 15:42:36 +0300

    feat: use prefs repository

--- lib/bloc/main_page/main_page_bloc.dart
+++ lib/bloc/main_page/main_page_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_fight_club/repository/shared_pref_repository.dart';
 import 'package:get/get.dart';
 import 'package:meta/meta.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'main_page_event.dart';
 part 'main_page_state.dart';
@@ -10,8 +10,8 @@ class MainPageBloc extends Bloc<MainPageEvent, MainPageState> {
   MainPageBloc() : super(MainPageState()) {
     on<MainPageSetWinner>((event, emit) async {
       if (event.fightResult != null) {
-        await Get.find<SharedPreferences>()
-            .setString('last_fight_result', event.fightResult!);
+        await Get.find<SharedPrefRepository>()
+            .serFightResult(event.fightResult!);
       }
 
       emit(state.copyWith(fightResult: event.fightResult));
