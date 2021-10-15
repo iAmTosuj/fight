98cc8b20956d708c6da305e287220c2d0844d23c
Tosuj <sanches00799@gmail.com>
2021-10-15 20:52:31 +0300

    feat: create splash screen

---
+++ lib/ui/splash/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fight_club/core/models/fooderlich_pages.dart';
+import 'package:flutter_fight_club/route/app_state_manager.dart';
+import 'package:provider/provider.dart';
+
+class SplashScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: FooderlichPages.splashPath,
+      key: ValueKey(FooderlichPages.splashPath),
+      child: SplashScreen(),
+    );
+  }
+
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Provider.of<AppStateManager>(context, listen: false).initializeApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [const Text('Initializing...')],
+        ),
+      ),
+    );
+  }
+}
