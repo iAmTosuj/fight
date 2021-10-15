aa2f8b8b2bf06baf904e0a2159d7375718d71b8a
Tosuj <sanches00799@gmail.com>
2021-10-15 20:53:29 +0300

    feat: add app path

---
+++ lib/core/models/fooderlich_pages.dart
@@ -0,0 +1,6 @@
+class FooderlichPages {
+  static String splashPath = '/splash';
+  static String home = '/';
+  static String fightPath = '/fight';
+  static String statisticPath = '/statistic';
+}
